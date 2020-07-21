28165d8b6247ce49a7e7839e059d690719d869a2
franck <franck.rst@gmail.com>
2020-07-21 20:51:28 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,22 @@ import * as cors from "cors";
 import {Server} from "./server";
 
 const app = express();
-app.use(cors());
+var allowedOrigins = ['http://localhost:4200',
+    'http://127.0.0.1:4200',
+    'https://rallypoint.tech'];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.set("port", process.env.PORT || 3000);
 
 new Server(app);
