0ed442086a0e917959a93e14cd506056e98ad50f
franck <franck.rst@gmail.com>
2020-07-21 17:51:53 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
+import * as cors from "cors";
 import {Server} from "./server";
 
 const app = express();
+app.use(cors());
 app.set("port", process.env.PORT || 3000);
 
 new Server(app);
